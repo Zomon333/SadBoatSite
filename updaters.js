1ea61a215f59b65cb9ae45656833b93092b00246
Dagan Poulin <dagan.poulin@sadboat.com>
2022-09-03 09:34:59 -0400

    Adding files

---
+++ updaters.js
@@ -0,0 +1,44 @@
+function home()
+{
+    document.getElementById("contentBox").src="subpages/home.html";
+
+}
+
+function news()
+{
+    document.getElementById("contentBox").src="subpages/news.html";
+}
+
+function about()
+{
+    document.getElementById("contentBox").src="subpages/about.html";
+
+}
+
+function contact()
+{
+    document.getElementById("contentBox").src="subpages/contact.html";
+
+}
+
+function store()
+{
+    document.getElementById("contentBox").src="subpages/store.html";
+
+}
+
+
+/*
+These buttons are for future use in the footer, if needed
+*/
+function legal()
+{
+    document.getElementById("contentBox").src="subpages/legal.html";
+
+}
+
+function copyright()
+{
+    document.getElementById("contentBox").src="subpages/copyright.html";
+
+}
