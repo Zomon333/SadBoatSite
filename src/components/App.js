ba0f26738b4486b364a54ecb7aed285a5845eb3d
Zomon333 <zomon333@gmail.com>
2022-11-17 02:10:08 -0500

    Removing code for ColorPicker in App

--- src/components/App.js
+++ src/components/App.js
@@ -55,53 +55,8 @@ function App() {
   const navbarColor="#272635ff";
   const sidebarColor="#272635ff";
 
-  //Converts RGB ints into hex
-  const getColor = (r, g, b)=>
-  {
-    const toHex = (arg) =>
-    {
-      const rBit = arg%16;
-      const lBit = arg-rBit;
-      
-      return (lBit/16).toString(16).concat(rBit.toString(16));
-    }
-    
-    return ("#".concat(toHex(r), toHex(g), toHex(b)));
-  }
-
-  //Stores the RGB values of the app
-  const [r, setR] = useState(0);
-  const [g, setG] = useState(0);
-  const [b, setB] = useState(0);
-  const [hasDrawn, setHasDrawn] = useState(false);
-
   //Describes the background of the app
   const [bgColor, setBgColor] = useState("#3f3d4a");
-
-  //Updates the background color if R,G,B updates
-  /*useEffect(
-    ()=>{
-      if(hasDrawn)
-      {
-        setBgColor(getColor(r,g,b));
-      }
-      setHasDrawn(true);
-    },[r,g,b]
-  )*/
-
-  const setters={
-    setR:setR,
-    setG:setG,
-    setB:setB,
-  }
-  const values={
-    r:r,
-    g:g,
-    b:b,
-  }
-
-
-  
 
   return (<>
   <section style={{backgroundColor:bgColor, width:"100vw", minHeight:"110vh", paddingBottom:"5em"}}>
